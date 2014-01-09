7e46b2e66e0aea4dc8d12799ed53bee000397239
Sutherland Boswell <sutherland.boswell@gmail.com>
2014-01-09 13:15:23 -0600

    Fix for Funny or Die thumbnails by fetching URL using oEmbed

--- php/providers/class-funnyordie-thumbnails.php
+++ php/providers/class-funnyordie-thumbnails.php
@@ -41,19 +41,27 @@ class Funnyordie_Thumbnails extends Video_Thumbnails_Providers {
 
 	// Thumbnail URL
 	public function get_thumbnail_url( $id ) {
-		return 'http://assets0.ordienetworks.com/tmbs/' . $id . '/large_11.jpg';
+		$request = "http://www.funnyordie.com/oembed.json?url=http%3A%2F%2Fwww.funnyordie.com%2Fvideos%2F$id";
+		$response = wp_remote_get( $request, array( 'sslverify' => false ) );
+		if( is_wp_error( $response ) ) {
+			$result = new WP_Error( 'funnyordie_info_retrieval', __( 'Error retrieving video information from the URL <a href="' . $request . '">' . $request . '</a> using <code>wp_remote_get()</code><br />If opening that URL in your web browser returns anything else than an error page, the problem may be related to your web server and might be something your host administrator can solve.<br />Details: ' . $response->get_error_message() ) );
+		} else {
+			$result = json_decode( $response['body'] );
+			$result = $result->thumbnail_url;
+		}
+		return $result;
 	}
 
 	// Test cases
 	public $test_cases = array(
 		array(
 			'markup' => '<iframe src="http://www.funnyordie.com/embed/5325b03b52" width="640" height="400" frameborder="0"></iframe>',
-			'expected' => 'http://assets0.ordienetworks.com/tmbs/5325b03b52/large_11.jpg',
+			'expected' => 'http://t.fod4.com/t/5325b03b52/c480x270_17.jpg',
 			'name' => 'iFrame player'
 		),
 		array(
 			'markup' => '<object width="640" height="400" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" id="ordie_player_5325b03b52"><param name="movie" value="http://player.ordienetworks.com/flash/fodplayer.swf" /><param name="flashvars" value="key=5325b03b52" /><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always"><embed width="640" height="400" flashvars="key=5325b03b52" allowfullscreen="true" allowscriptaccess="always" quality="high" src="http://player.ordienetworks.com/flash/fodplayer.swf" name="ordie_player_5325b03b52" type="application/x-shockwave-flash"></embed></object>',
-			'expected' => 'http://assets0.ordienetworks.com/tmbs/5325b03b52/large_11.jpg',
+			'expected' => 'http://t.fod4.com/t/5325b03b52/c480x270_17.jpg',
 			'name' => 'Flash player'
 		),
 	);

837a7d6ae2c6afa6419989edbd5bb7f0ca83fdf8
Sutherland Boswell <sutherland.boswell@gmail.com>
2014-06-04 15:15:17 -0500

    Finished implementing Tudou as a provider

--- php/providers/class-tudou-thumbnails.php
+++ php/providers/class-tudou-thumbnails.php
@@ -28,6 +28,17 @@ class Tudou_Thumbnails extends Video_Thumbnails_Providers {
 	public $service_slug = 'tudou';
 	const service_slug = 'tudou';
 
+	public $options_section = array(
+		'description' => '<p><strong>Optional</strong>: Only required if using videos from Tudou.</p><p><strong>Directions</strong>: Go to <a href="http://open.tudou.com/">open.tudou.com</a> and create an application, then copy and paste your app key below.</p>',
+		'fields' => array(
+			'app_key' => array(
+				'name' => 'App Key',
+				'type' => 'text',
+				'description' => ''
+			)
+		)
+	);
+
 	public static function register_provider( $providers ) {
 		$providers[self::service_slug] = new self;
 		return $providers;
@@ -35,11 +46,32 @@ class Tudou_Thumbnails extends Video_Thumbnails_Providers {
 
 	// Regex strings
 	public $regexes = array(
-		'#//(?:www\.)?tudou\.com/programs/view/html5embed\.action\?type=(?:[0-9]+)&code=([A-Za-z0-9_-]+)#', // iFrame SRC
+		'#//(?:www\.)?tudou\.com/programs/view/html5embed\.action\?type=(?:[0-9]+)(?:&|&\#038;|&amp;)code=([A-Za-z0-9\-_]+)#', // iFrame SRC
 	);
 
 	// Thumbnail URL
 	public function get_thumbnail_url( $id ) {
+		// Get our API key
+		$app_key = ( isset( $this->options['app_key'] ) && $this->options['app_key'] != '' ? $this->options['app_key'] : false );
+
+		if ( $app_key ) {
+			$request = "http://api.tudou.com/v6/video/info?app_key=$app_key&format=json&itemCodes=$id";
+			$response = wp_remote_get( $request, array( 'sslverify' => false ) );
+			if( is_wp_error( $response ) ) {
+				$result = $this->construct_info_retrieval_error( $request, $response );
+			} else {
+				$result = json_decode( $response['body'] );
+				if ( isset( $result->error_info ) ) {
+					$result = new WP_Error( 'tudou_api_error', $result->error_info );
+				} elseif ( !isset( $result->results[0]->bigPicUrl ) ) {
+					$result = new WP_Error( 'tudou_not_found',  sprintf( __( 'Unable to retrieve thumbnail for Tudou video with an ID of %s', 'video-thumbnails' ), $id ) );
+				} else {
+					$result = $result->results[0]->bigPicUrl;
+				}
+			}
+		} else {
+			$result = new WP_Error( 'tudou_api_key', __( 'You must enter an API key in the <a href="' . admin_url( 'options-general.php?page=video_thumbnails&tab=provider_settings' ) . '">provider settings</a> to retrieve thumbnails from Tudou.', 'video-thumbnails' ) );
+		}
 		return $result;
 	}
 
@@ -48,8 +80,8 @@ class Tudou_Thumbnails extends Video_Thumbnails_Providers {
 		return array(
 			array(
 				'markup'        => '<iframe src="http://www.tudou.com/programs/view/html5embed.action?type=1&code=V-TeNdhKVCA&lcode=eNoG-G9OkrQ&resourceId=0_06_05_99" allowtransparency="true" scrolling="no" border="0" frameborder="0" style="width:480px;height:400px;"></iframe>',
-				'expected'      => '',
-				'expected_hash' => '',
+				'expected'      => 'http://g3.tdimg.com/83fedbc41cf9055dce9182a0c07da601/w_2.jpg',
+				'expected_hash' => '3a5e656f8c302ae5b23665f22d296ae1',
 				'name'          => __( 'iFrame Embed', 'video-thumbnails' )
 			),
 		);
--- php/providers/class-video-thumbnails-providers.php
+++ php/providers/class-video-thumbnails-providers.php
@@ -115,6 +115,7 @@ require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-mpora-thumbnails.php
 require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-wistia-thumbnails.php' );
 // require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-kaltura-thumbnails.php' );
 require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-youku-thumbnails.php' );
+require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-tudou-thumbnails.php' );
 require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-collegehumor-thumbnails.php' );
 require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-rutube-thumbnails.php' );
 require_once( VIDEO_THUMBNAILS_PATH . '/php/providers/class-sapo-thumbnails.php' );

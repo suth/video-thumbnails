921e40022dbd0002c5cf1375f771a7cef1d09163
Sutherland Boswell <sutherland.boswell@gmail.com>
2016-06-28 15:02:26 -0500

    Test all the providers

--- tests/test-video-thumbnails-providers.php
+++ tests/test-video-thumbnails-providers.php
@@ -1,6 +1,24 @@
 <?php
 /**
- * @coversDefaultClass Video_Thumbnails_Providers
+ * @covers CollegeHumor_Thumbnails
+ * @covers Dailymotion_Thumbnails
+ * @covers Facebook_Thumbnails
+ * @covers FunnyOrDie_Thumbnails
+ * @covers GoogleDrive_Thumbnails
+ * @covers Livestream_Thumbnails
+ * @covers Metacafe_Thumbnails
+ * @covers Mpora_Thumbnails
+ * @covers Rutube_Thumbnails
+ * @covers Sapo_Thumbnails
+ * @covers Ted_Thumbnails
+ * @covers Tudou_Thumbnails
+ * @covers Twitch_Thumbnails
+ * @covers Vimeo_Thumbnails
+ * @covers Vine_Thumbnails
+ * @covers Vk_Thumbnails
+ * @covers Wistia_Thumbnails
+ * @covers Youku_Thumbnails
+ * @covers YouTube_Thumbnails
  */
 class Video_Thumbnails_Provider_Test extends WP_UnitTestCase {
 
@@ -20,7 +38,6 @@ class Video_Thumbnails_Provider_Test extends WP_UnitTestCase {
 
     /**
      * @test
-     * @covers ::parse
      */
     public function it_parses_content_and_returns_an_array()
     {
@@ -28,4 +45,59 @@ class Video_Thumbnails_Provider_Test extends WP_UnitTestCase {
 
         $this->assertEquals( array(), $results );
     }
+
+    /**
+     * @test
+     */
+    public function providers_all_parse_content()
+    {
+        foreach ($this->providers->get_providers() as $provider) {
+            $this->assertProviderMatches($provider);
+        }
+    }
+
+    public function assertProviderMatches($provider) {
+        foreach ($provider->get_tests() as $test) {
+            $videos = $provider->parse($test['markup']);
+            $this->assertEquals(
+                $test['expected'],
+                $videos[0]->data,
+                'Failed to correctly parse ' . $provider->name . ': ' . $test['name']
+            );
+        }
+    }
+
+    /**
+     * @test
+     * @ignore
+     * @group external-http
+     */
+    public function providers_all_fetch_image_urls()
+    {
+        foreach ($this->providers->get_providers() as $provider) {
+            $this->assertProviderFetches($provider);
+        }
+    }
+
+    public function assertProviderFetches($provider) {
+        foreach ($provider->get_tests() as $test) {
+            $url = $provider->get_thumbnail_url($test['expected']);
+            var_dump($url);
+            $this->assertEquals(
+                $test['image_hash'],
+                $this->getFileHash( $url ),
+                'Failed to correctly fetch ' . $provider->name . ' video: ' . $test['expected']
+            );
+        }
+    }
+
+    public function getFileHash( $url ) {
+		$response = wp_remote_get( $url );
+		if( is_wp_error( $response ) ) {
+			$result = false;
+		} else {
+			$result = md5( $response['body'] );
+		}
+		return $result;
+	}
 }

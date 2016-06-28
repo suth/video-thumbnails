8004a09244a731816128b55e7b4ac0f52534d233
Sutherland Boswell <sutherland.boswell@gmail.com>
2016-06-28 16:17:11 -0500

    More media library tests

--- tests/test-video-thumbnails-media-library.php
+++ tests/test-video-thumbnails-media-library.php
@@ -1,23 +1,48 @@
 <?php
 /**
- * @coversDefaultClass Video_Thumbnails_Media_Library
+ * @covers Video_Thumbnails_Media_Library
  */
 class Video_Thumbnails_Media_Library_Test extends WP_UnitTestCase {
+    private function invokePrivateMethod($methodName, $args = array())
+    {
+		$reflector = new ReflectionClass('Video_Thumbnails_Media_Library');
+		$method = $reflector->getMethod($methodName);
+		$method->setAccessible(true);
+
+		return $method->invokeArgs(new Video_Thumbnails_Media_Library, $args);
+    }
+
     /**
      * @test
-     * @covers ::__construct
-     * @covers ::get_image_url
      */
-    public function it_can_get_an_image()
+    public function it_can_get_extension_for_image_types()
     {
-        $match = new Video_Thumbnails_Match(
-            'id_data',
-            function( $data ) { return $data . '.jpg'; }
+        $types = array(
+            'image/jpeg' => '.jpg',
+            'image/png' => '.png',
+            'image/gif' => '.gif'
         );
 
+        foreach ($types as $type => $extension) {
+            $this->assertEquals(
+                $extension,
+                $this->invokePrivateMethod('get_extension_for_content_type', array($type))
+            );
+        }
+
+        $this->assertWPError(
+            $this->invokePrivateMethod('get_extension_for_content_type', array('application/json'))
+        );
+    }
+
+    /** @test */
+    public function it_can_generate_a_file_name_for_a_post()
+    {
+        $post = $this->factory->post->create( array( 'post_title' => 'This is a Sample' ) );
+
         $this->assertEquals(
-            'id_data.jpg',
-            $match->get_image_url()
+            'this-is-a-sample.jpg',
+            $this->invokePrivateMethod('construct_filename', array($post, '.jpg'))
         );
     }
 }

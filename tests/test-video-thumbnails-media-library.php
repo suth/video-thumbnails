3c01e213d01dc315c54d10160431059cb5868dde
Sutherland Boswell <sutherland.boswell@gmail.com>
2016-06-28 15:55:39 -0500

    More tests

---
+++ tests/test-video-thumbnails-media-library.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @coversDefaultClass Video_Thumbnails_Media_Library
+ */
+class Video_Thumbnails_Media_Library_Test extends WP_UnitTestCase {
+    /**
+     * @test
+     * @covers ::__construct
+     * @covers ::get_image_url
+     */
+    public function it_can_get_an_image()
+    {
+        $match = new Video_Thumbnails_Match(
+            'id_data',
+            function( $data ) { return $data . '.jpg'; }
+        );
+
+        $this->assertEquals(
+            'id_data.jpg',
+            $match->get_image_url()
+        );
+    }
+}
+

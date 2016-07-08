4c03421b972132ccf170885cee847e01aa0403d7
Sutherland Boswell <sutherland.boswell@gmail.com>
2016-07-08 13:39:54 -0500

    Fake test

--- tests/test-video-thumbnails-media-library.php
+++ tests/test-video-thumbnails-media-library.php
@@ -3,5 +3,10 @@
  * @covers Video_Thumbnails_Media_Library
  */
 class Video_Thumbnails_Media_Library_Test extends WP_UnitTestCase {
+    /** @test */
+    public function it_tests()
+    {
+        $this->assertTrue(true);
+    }
 }
 
